10cd77664264e07d643f9a19ee7182bd8c078fdc
Erik Schoster <erik@hecanjog.com>
2014-05-25 02:09:30 -0500

    oops

--- app.py
+++ app.py
@@ -6,22 +6,16 @@ import pytumblr
 import re
 import os
 import sys
-import glob
 
 app = Flask(__name__)
 
 @app.route('/webgl')
 def webgl():
-    js = {
-        'js': glob.glob('static/js/*.js'),
-        'lib': glob.glob('static/lib/*.js'),
-    }
-
-    return render_template('webgl.html', js=js)
+    return render_template('webgl.html')
 
 @app.route('/')
 def index():
-    return render_template('index.html', js=js)
+    return render_template('index.html')
 
 @app.route('/<swk>/<swa>/<nek>/<nea>')
 def getPosts(swk, swa, nek, nea):
