766b4a98815444df1571107c3f0265bd47e3ef8e
PRF <paul.feyertag@gmail.com>
2014-07-06 01:49:42 -0500

    trying out gulp for build

---
+++ wctank/static/gulpfile.js
@@ -0,0 +1,24 @@
+/*
+ * Trying gulp to manage build process
+ * 
+ * For the moment, not including npm devdependencies because
+ * of path name length restrictions in windows, which makes dealing 
+ * with deeply nested node_module folders a PITA in git. So, until I switch 
+ * enviornments, let's leave that stuff out...
+ * 
+ * Requires gulp, gulp-concat, gulp-uglify - compiler.jar should
+ * be in this directory (for the time being).
+ * 
+ * Then, just cd into this directory and run gulp
+ * 
+ */
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', function() {
+	return gulp.src(['./lib/*.js','./js/*.js'])
+		.pipe(concat('webgl.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./build/'));
+});
