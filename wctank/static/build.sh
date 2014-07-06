6405e9af61dd06fa0e575b6ec36e445d33511eea
PRF <paul.feyertag@gmail.com>
2014-07-06 15:46:36 -0500

    switched to sh for build

---
+++ wctank/static/build.sh
@@ -0,0 +1,12 @@
+echo COMPILING...
+JS=`find ./lib/*.js`
+LIB=`find ./js/*.js`
+echo $JS
+echo $LIB
+java -jar ./devdeps/compiler.jar --js $JS $LIB --js_output_file ./build/webgl.js \
+      --language_in ECMASCRIPT5 \
+      #--compilation_level ADVANCED_OPTIMIZATIONS --externs ./devdeps/externs.js --create_source_map ./devdeps/map.js
+echo Done.
+#echo APPENDING DEBUGTAG
+#echo '//@ sourceMappingURL=../map.js' >> ./build/webgl.js
+#echo Done.
