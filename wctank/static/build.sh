09b4220100df32f277708d342dee76543f8ab1c3
PRF <paul.feyertag@gmail.com>
2014-07-06 15:49:21 -0500

    oop

--- wctank/static/build.sh
+++ wctank/static/build.sh
@@ -8,5 +8,5 @@ java -jar ./devdeps/compiler.jar --js $JS $LIB --js_output_file ./build/webgl.js
       #--compilation_level ADVANCED_OPTIMIZATIONS --externs ./devdeps/externs.js --create_source_map ./devdeps/map.js
 echo Done.
 #echo APPENDING DEBUGTAG
-#echo '//@ sourceMappingURL=../map.js' >> ./build/webgl.js
+#echo '//@ sourceMappingURL=../devdeps/map.js' >> ./build/webgl.js
 #echo Done.
