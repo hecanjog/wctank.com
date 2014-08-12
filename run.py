74cbb477cfe7c50d33a142fe8c7b0d12f09d79e2
PRF <paul.feyertag@gmail.com>
2014-08-12 16:32:25 -0500

    harumpf

--- run.py
+++ run.py
@@ -3,8 +3,4 @@ import sys
 
 if __name__ == '__main__':
     app.debug = True
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'paul':
-            app.paul = True
-            print("paul's machine mode")
     app.run('0.0.0.0')
