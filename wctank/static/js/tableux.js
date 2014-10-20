2e35041a679238160ae8cf305181101bb089e8fa
rocksynthetic <paul.feyertag@gmail.com>
2014-10-19 23:34:32 -0500

    add a couple of tableux

--- wctank/static/js/tableux.js
+++ wctank/static/js/tableux.js
@@ -16,7 +16,8 @@ wctank.tableux = (function(tableux) {
             PRINT_ANALOG: 0x0F000000,
             CAUSTIC_GLOW: 0x00F00000,
             CMGYK:        0x000F0000,
-            VHS:          0x0000F000
+            VHS:          0x0000F000,
+            ALL:          0x0FFFF000
         };
         var makeTableuxData = function(lat, lng, zoom, tag) {
             return {
@@ -28,8 +29,16 @@ wctank.tableux = (function(tableux) {
         dat.locs = [
             makeTableuxData(43.035578033062414, -87.92508879368779, 20, tags.PRINT_ANALOG),
             makeTableuxData(42.70103069787964, -87.99994131176345, 18, tags.CAUSTIC_GLOW),
-            makeTableuxData(41.73787991072762, -87.47784991638764, 16, tags.CMGYK), 
-            makeTableuxData(42.75690051169562, -87.80957013350911, 20, tags.VHS)
+            makeTableuxData(41.73787991072762, -87.47784991638764, 16, tags.CMGYK | tags.CAUSTIC_GLOW), 
+            makeTableuxData(42.75690051169562, -87.80957013350911, 20, tags.VHS),
+            makeTableuxData(57.98276027896727, -122.41318245308503, 4, tags.PRINT_ANALOG | tags.CAUSTIC_GLOW),
+            makeTableuxData(41.73788064076405, -87.54661773794578, 20, tags.PRINT_ANALOG),
+            makeTableuxData(41.351808897930226, -89.22587973528789, 16, 
+                            tags.VHS | tags.CAUSTIC_GLOW | tags.PRINT_ANALOG),
+            makeTableuxData(1.31960789248782, -89.7875060971246, 20, tags.VHS),
+            makeTableuxData(41.3207838120689, -89.8110525395141, 16, tags.VHS | tags.PRINT_ANALOG | tags.CMGYK),
+            // this one is nice consider maing fauvist a start
+            makeTableuxData(50.67774455640808, -111.7312581585038, 19, tags.ALL) 
         ];
         (function() {
             for (var i = 0; i < dat.locs.length; i++) {
