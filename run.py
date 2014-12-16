d57de5a94d7f98508808c40efb163c0a9282d16f
rocksynthetic <paul.feyertag@gmail.com>
2014-12-16 06:25:39 -0600

    schedule hourly updating of post database

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
-from wctank import app
+from wctank import app, db, populate
+from scheduler import scheduler
 
 if __name__ == '__main__':
+    # doesn't do anything if database already exists
+    db.create_all()
+
+    # doesn't overwrite extant posts
+    populate.updateDb()
+
+    scheduler.start()
+
     app.debug = False
     app.run('0.0.0.0')
+
--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     install_requires=[
         'flask', 
         'pytumblr',
-        'ZODB',
+        'Flask-SQLAlchemy',
+        'apscheduler'
     ],
     zip_safe=False,
 )
