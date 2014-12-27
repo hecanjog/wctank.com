d0dc34800bae0706f79c21089924b47a37a0a74a
rocksynthetic <paul.feyertag@gmail.com>
2014-12-26 23:15:55 -0600

    oops fix populate.updateDb call before db creation

--- wctank/scheduler.py
+++ wctank/scheduler.py
@@ -7,6 +7,8 @@ scheduler.add_jobstore('sqlalchemy', url='sqlite:///wctank/jobs.db')
 
 ids = [x.id for x in scheduler.get_jobs()]
 
-populate.updateDb()
+def firstPopulate():
+    populate.updateDb()
+
 if 'updatePostsDb' not in ids:
     scheduler.add_job(populate.updateDb, 'interval', hours=1, id='updatePostsDb')
